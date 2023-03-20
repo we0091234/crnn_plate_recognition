ef9a384c1f5afb13bf90907135ea0d753e978275
we0091234 <447587096@qq.com>
2023-03-20 21:39:01 +0800

    demo modify

--- demo_plate_color.py
+++ demo_plate_color.py
@@ -74,7 +74,7 @@ def init_model(device,model_path):
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_path', type=str, default=r'color_model_small/0.981308_epoth_86_model.pth', help='model.pt path(s)')  
+    parser.add_argument('--model_path', type=str, default=r'saved_model/plate_rec_color.pth', help='model.pt path(s)')  
     parser.add_argument('--image_path', type=str, default=r'/mnt/EPan/carPlate/@realTest2_noTraining/realrealTest/', help='source') 
     # parser.add_argument('--image_path', type=str, default=r'images', help='source') 
     parser.add_argument('--img_h', type=int, default=48, help='height') 
